8144d1ea5ba7e8e2267e42f38cd1ce15174a1cc1
PezpezRoundyeszzzz for cookie <yukupo32@gmail.com>
2022-04-18 16:56:14 +0800

    init

--- js/form.js
+++ js/form.js
@@ -1,3 +1,8 @@
 function notImplemented(){
 	alert("This function is not implemented.");
 }
+
+
+// function SumbitSuccessfulPage() {
+// 	window.location.href("http://127.0.0.1:5501/Sumbit-Successful.html");
+// }
